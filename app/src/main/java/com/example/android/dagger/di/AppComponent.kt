8aec10b86b68bc75a5ece8faf977880377793c51
sandeepsharma-kgp <sandeepsharma.iit@gmail.com>
2019-12-22 16:31:00 +0530

    done till scoping subcomponents

---
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -0,0 +1,31 @@
+package com.example.android.dagger.di
+
+import android.content.Context
+import com.example.android.dagger.login.LoginComponent
+import com.example.android.dagger.main.MainActivity
+import com.example.android.dagger.registration.RegistrationActivity
+import com.example.android.dagger.registration.RegistrationComponent
+import com.example.android.dagger.registration.enterdetails.EnterDetailsFragment
+import com.example.android.dagger.registration.termsandconditions.TermsAndConditionsFragment
+import com.example.android.dagger.settings.SettingsActivity
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [StorageModule::class, AppSubcomponents::class])
+interface AppComponent {
+    // Factory to create instances of the AppComponent
+    @Component.Factory
+    interface Factory {
+        // With @BindsInstance, the Context passed in will be available in the graph
+        fun create(@BindsInstance context: Context): AppComponent
+    }
+    // Classes that can be injected by this Component
+    fun inject(activity: MainActivity)
+    fun inject(activity: SettingsActivity)
+
+    // Expose RegistrationComponent factory from the graph
+    fun registrationComponent(): RegistrationComponent.Factory
+    fun loginComponent(): LoginComponent.Factory
+}
---
+++ app/src/main/java/com/example/android/dagger/registration/RegistrationComponent.kt
@@ -0,0 +1,26 @@
+package com.example.android.dagger.registration
+
+import com.example.android.dagger.di.ActivityScope
+import com.example.android.dagger.registration.enterdetails.EnterDetailsFragment
+import com.example.android.dagger.registration.termsandconditions.TermsAndConditionsFragment
+import dagger.Subcomponent
+
+// Scope annotation that the RegistrationComponent uses
+// Classes annotated with @ActivityScope will have a unique instance in this Component
+@ActivityScope
+// Definition of a Dagger subcomponent
+@Subcomponent
+interface RegistrationComponent {
+
+    // Factory to create instances of RegistrationComponent
+    @Subcomponent.Factory
+    interface Factory {
+        fun create(): RegistrationComponent
+    }
+
+    // Classes that can be injected by this Component
+    fun inject(activity: RegistrationActivity)
+    fun inject(fragment: EnterDetailsFragment)
+    fun inject(fragment: TermsAndConditionsFragment)
+
+}
